84147986db831d7b40d51e529cdc347fe9779bb9
Lilianada <lilibest30.lb@gmail.com>
2025-01-27 00:21:18 +0100

    switched out experience design

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,38 +3,51 @@ import { experiences } from "../utils/data";
 
 const Experience = () => {
   return (
-    <section className="lg:mb-32">
-      <div id="experience" className="lg:min-h-screen">
-        <div className="lg:hidden grid items-center col-span-2 grid-cols-[1fr_auto] gap-4 mb-12">
-          <span className="inline-block h-[1.5px] bg-current opacity-50 transition-all duration-300 w-full" />
-          <h2
-            className={`text-[20px] leading-[36px] font-semibold text-right lg:text-[70px] lg:font-[900] lg:leading-[70px] lg:w-3/4 ml-auto`}
-          >
-            <span>6 Years </span>
-            <span className="opacity-70">of Experience</span>{" "}
-          </h2>
-        </div>
-        
+    <section className="lg:mb-32 relative">
+      <div id="experience" className="lg:hidden grid items-center col-span-2 grid-cols-[1fr_auto] gap-4 mb-12">
+        <span className="inline-block h-[1.5px] bg-current opacity-50 transition-all duration-300 w-full" />
         <h2
-          className={`hidden lg:block text-[20px] leading-[36px] font-semibold mb-12 text-right lg:text-[70px] lg:font-[900] lg:leading-[70px] lg:w-3/4 ml-auto`}
-        >
+          className="text-[20px] leading-[36px] font-semibold text-right">
           <span>6 Years </span>
-          <span className="opacity-70">of Experience</span>{" "}
+          <span className="opacity-70">of Experience</span>
         </h2>
+      </div>
+      <h2 className={`hidden lg:block mb-12 text-right text-[70px] font-[900] leading-[70px] w-3/4 ml-auto`}>
+        <span>6 Years </span>
+        <span className="opacity-70">of Experience</span>
+      </h2>
 
+      <div className="lg:hidden relative pl-4 min-h-screen">
         {experiences.map((exp, index) => (
-          <div key={index} className="mb-10 lg:flex lg:mb-6 lg:p-4">
-            <p className={`text-xs leading-4 lg:w-1/4 lg:mt-[6px]`}>
+           <div 
+           key={index} 
+           className="relative pl-4 last:pb-0 mb-4"
+         >
+           <div className="absolute left-[-20px] top-0 bottom-0 flex flex-col items-center gap-2 mt-2">
+             <div className="w-2 h-2 bg-current rounded-full"></div>
+             <div className="w-[1px] h-full bg-current opacity-30" ></div>
+           </div>
+
+           <div className="">
+             <h3 className="font-semibold text-lg leading-6">{exp.company}</h3>
+             <p className="text-xs mt-1 opacity-50">{exp.period}</p>
+             <p className="mt-3 text-xs tracking-widest uppercase">
+               {exp.role}
+             </p>
+             <p className="text-sm mt-1 leading-6 opacity-80">{exp.description}</p>
+           </div>
+         </div>
+        ))}
+      </div>
+
+      {/* desktop */}
+      <div className="hidden lg:block">
+        {experiences.map((exp, index) => (
+          <div key={index} className="flex mb-6 p-4">
+            <p className={`text-xs leading-4 w-1/4 mt-[6px]`}>
               {exp.period}
             </p>
-            <p className="mt-2 leading-4 text-xs lg:hidden opacity-50 tracking-widest uppercase">
-              {exp.role}
-            </p>
-            <h3 className={`mt-1 font-semibold text-lg lg:hidden`}>
-              {exp.company}
-            </h3>
-            {/* desktop */}
-            <div className="hidden lg:flex flex-col w-3/4">
+            <div className="w-3/4">
               <h3 className="font-semibold text-[20px] leading-6">
                 {exp.company}
               </h3>
@@ -43,10 +56,6 @@ const Experience = () => {
               </p>
               <p className=" opacity-80">{exp.description}</p>
             </div>
-
-            <p className="lg:hidden text-sm leading-5 opacity-80 mt-3">
-              {exp.description}
-            </p>
           </div>
         ))}
       </div>
